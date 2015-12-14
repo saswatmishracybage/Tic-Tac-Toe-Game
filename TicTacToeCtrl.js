a20ca613d9f6300e9943536058ffba21d57a39e3
saswatmishracybage <saswatmi@cybage.com>
2015-12-14 14:59:30 +0530

    TicTacToe commit after Anand's review.

--- TicTacToeCtrl.js
+++ TicTacToeCtrl.js
@@ -1,11 +1,6 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/* 
-    Created on : Dec 14, 2015, 11:33:28 AM
-    Author     : saswatmi
+    @Created on : Dec 14, 2015, 11:33:28 AM
+    @Author     : saswatmi
 */
 /**
  * Tic Tac Toe Module
@@ -20,12 +15,13 @@ var ticTacToeApp = angular.module( 'ticTacToeApp', [] );
  * @param {obj} $scope
  * @param {function} function 
  */
-ticTacToeApp.controller( 'TicTacToeController', ['$scope', function ($scope) {
+ticTacToeApp.controller( 'TicTacToeController', ['$scope', function ( $scope ) {
 
     $scope.currentPlayer = 'O';
     $scope.player = 'O';
     $scope.winner = null;
     $scope.boardSize = 3;
+    $scope.boardSizeOptions = [3, 4, 5, 6, 7, 8, 9, 10];
     $scope.boardIndex = [0, 1, 2];
     $scope.board = [
         [null, null, null],
@@ -40,44 +36,45 @@ ticTacToeApp.controller( 'TicTacToeController', ['$scope', function ($scope) {
     $scope.changeBoardSize = function () {
         $scope.boardIndex = [];
         $scope.board = [];
-        for (var rowIndex = 0; rowIndex < $scope.boardSize; rowIndex++) {
-            $scope.boardIndex.push(rowIndex);
+        for ( var rowIndex = 0; rowIndex < $scope.boardSize; rowIndex++ ) {
+            $scope.boardIndex.push( rowIndex );
             $scope.board[rowIndex] = [];
-            for (var columnIndex = 0; columnIndex < $scope.boardSize; columnIndex++) {
-                $scope.board[rowIndex].push(null);
+            for ( var columnIndex = 0; columnIndex < $scope.boardSize; columnIndex++ ) {
+                $scope.board[rowIndex].push( null );
             }
         }
         $scope.newGame();
     };
 /**
- * 
+ * This function returns the value of each cell based on the index
  * @param {number} row
  * @param {number} column
- * return the value of cell in the board
+ * @return the {string} of cell in the board
  */
-    function cell(row, column) {
+    function cell( row, column ) {
         return $scope.board[row][column];
     };
 /**
- * 
+ * This functin decide the return value of each cell
+ * default is '-'
  * @param {number} row
  * @param {number} column
- * @returns the value of cell on each click
+ * @returns {stringt} the value of cell on each click
  */    
 
-    $scope.cellText = function (row, column) {
+    $scope.cellText = function ( row, column ) {
         var value = cell(row, column);
         return value ? value : '-';
     };
 /**
- * 
+ * On each click of the cell it set the value to the cell
+ * Alternate the players
+ * decides who is the winner
  * @param {number} row
  * @param {number} column
- * on each click of the cell it set the value to the cell
- * alternate the players
- * decides who is the winner
+ * 
  */
-    $scope.cellClick = function (row, column) {
+    $scope.cellClick = function ( row, column ) {
         if( $scope.winner ) {
             $scope.result = true;
             return false;
@@ -97,91 +94,92 @@ ticTacToeApp.controller( 'TicTacToeController', ['$scope', function ($scope) {
     };
     
 /**
- * 
+ * This function set the value of the cell
  * @param {number} row
  * @param {number} column
  * @param {string} value
- * set the value to the cell of the board
  */
 
-    function setCell(row, column, value) {
+    function setCell( row, column, value ) {
         $scope.board[row][column] = value;
 
     };
 /**
- * 
- * @param {number} row
- * @param {number} column
  * on each click it check the value horizontally, vertically and diagonally
  * declares the winner if any.
+ * @param {number} row
+ * @param {number} column
+ * checkVertical variable builts the string to check the values along vertical direction
+ * checkHorizontal variable builts the string to check the values along horizontal direction
+ * checkRightToLeft variable builts the string to check the values along right to left diagonal
+ * checkLeftToRight variable builts the string to check the values along left to right diagonal
+ * checkHorizontalAnd and checkVerticalAnd variables concatenate the AND condition to the main string
  */
     function checkBoard( row, column ) {
         var winner, empty = false;
 
         // check for any empty cell
         for ( var i = 0; i < $scope.boardSize; i++ ) {
-            for (var j = 0; j < $scope.boardSize; j++) {
-                if (!cell(i, j))
+            for ( var j = 0; j < $scope.boardSize; j++ ) {
+                if ( !cell( i, j ) )
                     empty = true;
             }
         }
 
         // check board vertically and horizontally  
-        var horizonCondt = '';
-        var verticalCondt = '';
+        var checkHorizontal = '';
+        var checkVertical = '';
         for (var index = 0; index < $scope.boardSize; index++) {
-            horizonCondt = "cell(" + index + ", 0)";
-            verticalCondt = "cell( 0, " + index + ")";
+            checkHorizontal = "cell(" + index + ", 0)";
+            checkVertical = "cell( 0, " + index + ")";
 
             for (var j = 1; j < $scope.boardSize; j++) {
                 if (j > 1) {
-                    horizonAndCondt = horizonCondt.split('==')[1];
-                    verticAndlCondt = verticalCondt.split('==')[1];
+                    var checkHorizontalAnd = checkHorizontal.split('==')[1];
+                    var checkVerticalAnd = checkVertical.split('==')[1];
                 } else {
-                    horizonAndCondt = horizonCondt;
-                    verticAndlCondt = verticalCondt;
+                    var checkHorizontalAnd = checkHorizontal;
+                    var checkVerticalAnd = checkVertical;
                 }
-
-                horizonCondt = horizonCondt + " && " + horizonAndCondt + " == cell(" + index + "," + j + " )";
-                verticalCondt = verticalCondt + " && " + verticAndlCondt + " == cell(" + j + "," + index + " )";
+                checkHorizontal = checkHorizontal + " && " + checkHorizontalAnd + " == cell(" + index + "," + j + " )";
+                checkVertical = checkVertical + " && " + checkVerticalAnd + " == cell(" + j + "," + index + " )";
             }
 
-            if ( eval( horizonCondt ) ) {
+            if ( eval( checkHorizontal ) ) {
                 winner = cell(index, 0);
             }
 
-            if ( eval( verticalCondt ) ) {
+            if ( eval( checkVertical ) ) {
                 winner = cell(0, index);
             }
-
         }
 
         // check board diagonally
-        var strRightToLeft = '';
-        var strLeftToRight = '';
+        var checkRightToLeft = '';
+        var checkLeftToRight = '';
         var j = $scope.boardSize - 1;
         for (var i = 0; i < $scope.boardSize; i++) {
             if (i == 0) {
-                strRightToLeft += "cell(" + i + "," + j + ") && cell(" + i + "," + j + ")";
-                strLeftToRight = "cell(" + i + "," + i + ") && cell(" + i + "," + i + ")"
+                checkRightToLeft += "cell(" + i + "," + j + ") && cell(" + i + "," + j + ")";
+                checkLeftToRight = "cell(" + i + "," + i + ") && cell(" + i + "," + i + ")"
             } else {
-                if (i + 1 == $scope.boardSize) {
-                    strRightToLeft += "== cell(" + i + "," + j + ")";
-                    strLeftToRight += "== cell(" + i + "," + i + ")";
+                if ( i + 1 == $scope.boardSize ) {
+                    checkRightToLeft += "== cell(" + i + "," + j + ")";
+                    checkLeftToRight += "== cell(" + i + "," + i + ")";
                 } else {
-                    strRightToLeft += "== cell(" + i + "," + j + ") && cell(" + i + "," + j + ")";
-                    strLeftToRight += "== cell(" + i + "," + i + ") && cell(" + i + "," + i + ")";
+                    checkRightToLeft += "== cell(" + i + "," + j + ") && cell(" + i + "," + j + ")";
+                    checkLeftToRight += "== cell(" + i + "," + i + ") && cell(" + i + "," + i + ")";
                 }
             }
             j--;
-
         }
         
-        if (eval(strRightToLeft)) {
-            winner = cell(0, $scope.boardSize - 1);
+        if ( eval( checkRightToLeft ) ) {
+            winner = cell( 0, $scope.boardSize - 1 );
         }
-        if (eval(strLeftToRight)) {
-            winner = cell(0, 0);
+        
+        if ( eval( checkLeftToRight ) ) {
+            winner = cell( 0, 0 );
         }
 
         // no more empty cell - no winner
@@ -210,13 +208,17 @@ ticTacToeApp.controller( 'TicTacToeController', ['$scope', function ($scope) {
         $scope.player = 'O';
         $scope.winner = null;
     };
-
 }]);
-
-ticTacToeApp.directive( 'winnerDirective', function() {
+/**
+ * 
+ * This is the custome directive intended to show the result of the match.
+ * @param {string} directive name
+ * @param {function} function
+ */
+ticTacToeApp.directive( 'gameResult', function() {
     var btn = '<input type="button" class="btn" ng-click="newGame()" value="New Game" />';
     return {
         restrict : 'EA',
-        template : '<h1>{{winner}} !! '+btn+'</h1>'
+        template : '<strong  class="alert alert-success">{{winner}} !!</strong>'+btn
     }
 })
